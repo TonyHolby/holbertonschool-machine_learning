b258c8a0fd0fa5151056c8d22889f0ad9aa30269
TonyHolby <tonynemoutheholberton@gmail.com>
2025-04-07 12:34:53 +0200

    Fix code for xticks configuration in 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -14,7 +14,7 @@ def frequency():
     plt.hist(student_grades, range=(0, 100), bins=10, edgecolor='black')
     plt.xlim(0, 100)
     plt.ylim(0, 30)
-    plt.xticks(range(0, 100, 10))
+    plt.xticks(range(0, 101, 10))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
