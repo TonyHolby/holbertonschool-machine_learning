0e70b052ce834543826fcdb3b4dac4a3014a2b34
TonyHolby <tonynemoutheholberton@gmail.com>
2025-04-07 09:45:20 +0200

    Write code in 0-line.py

---
+++ math/plotting/0-line.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A function that plots y as aline graph """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, 'r')
+    plt.show()
