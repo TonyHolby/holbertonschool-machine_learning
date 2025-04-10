9a4c2f9b2464a9d202e315f9b45ed712daa02bcc
TonyHolby <tonynemoutheholberton@gmail.com>
2025-04-10 15:12:03 +0200

    Write code in 17-integrale.py

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" A function that calculates the integral of a polynomial """
+
+
+def poly_integral(poly, C=0):
+    """
+        Calculates the integral of a polynomial.
+
+        Parameters:
+            poly (list): The list of coefficients representing a polynomial.
+            C (int): An integer representing the integration constant.
+
+        Return:
+            None, if poly or C are not valid.
+            A new list of coefficients representing
+            the integral of the polynomial, else.
+    """
+    if not isinstance(poly, list) or not isinstance(C, int):
+        return None
+
+    if not poly:
+        return None
+
+    if poly == [0]:
+        return[0]
+
+    n = 1
+    integral_coef = 0
+    new_list = [0]
+
+    for number in poly:
+        integral_coef = number * (1 / n)
+        if integral_coef == int(integral_coef):
+            new_list.append(int(integral_coef))
+        else:
+            new_list.append(integral_coef)
+        n += 1
+
+    return new_list
