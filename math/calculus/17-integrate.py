bbd577388c5296c53ee0385e432fd72f302d7904
TonyHolby <tonynemoutheholberton@gmail.com>
2025-04-12 11:05:58 +0200

    Fix the rounded coefficients in poly and variables in for loop in 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -18,26 +18,25 @@ def poly_integral(poly, C=0):
     if not isinstance(poly, list) or not isinstance(C, int):
         return None
 
+    for coefficients in poly:
+        if not isinstance(coefficients, (int, float)):
+            return None
+
     if not poly:
         return None
 
     if poly == [0]:
         return [C]
 
-    n = 0
-    integral_coef = 0
     new_list = [C]
-    number = 1
-
-    for number in range(len(poly)):
-        integral_coef = poly[number] * (1 / (n + 1))
+    for index in range(len(poly)):
+        integral_coef = poly[index] / (index + 1)
         if integral_coef == int(integral_coef):
             new_list.append(int(integral_coef))
-        elif abs(integral_coef * 100 % 100) < 1e-8:
-            new_list.append(round(integral_coef, 1))
+        elif abs(integral_coef - round(integral_coef)) < 1e-8:
+            new_list.append(round(integral_coef))
         else:
             new_list.append(integral_coef)
-        n += 1
 
     while len(new_list) > 1 and new_list[-1] == 0:
         new_list.pop()
