aaf20a4f36c556e5be5bd0ce0549495e2fab3cf8
TonyHolby <tonynemoutheholberton@gmail.com>
2025-04-20 11:50:14 +0200

    Fix output display in 7-build_decision_tree.py

--- supervised_learning/decision_tree/7-build_decision_tree.py
+++ supervised_learning/decision_tree/7-build_decision_tree.py
@@ -350,7 +350,7 @@ class Decision_Tree():
 - Depth                     : {self.depth()}
 - Number of nodes           : {self.count_nodes()}
 - Number of leaves          : {self.count_nodes(only_leaves = True)}
-- Accuracy on training data : {self.accuracy(self.explanatory, self.target)}
+- Accuracy on training data : {self.accuracy(self.explanatory, self.target)}\
 """)
 
     def np_extrema(self, arr):
