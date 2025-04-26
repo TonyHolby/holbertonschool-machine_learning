cbeb1e10f651c07ecc0104f6abe4334ec7ec9d7a
TonyHolby <tonynemoutheholberton@gmail.com>
2025-04-26 23:43:33 +0200

    Fix code in 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -22,17 +22,17 @@ class DeepNeuralNetwork:
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx <= 0:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, (list, int)) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for i in range(1, self.L):
+        for i in range(1, len(layers)):
             self.weights['W' + str(i)] = np.random.randn(
                 layers[i], layers[i-1]) * np.sqrt(2 / layers[i-1])
             self.weights['b' + str(i)] = np.zeros((layers[i], 1))
@@ -40,3 +40,4 @@ class DeepNeuralNetwork:
         self.weights['W' + str(self.L)] = np.random.randn(
             layers[self.L-1], layers[self.L-2]) * np.sqrt(2 / layers[self.L-2])
         self.weights['b' + str(self.L)] = np.zeros((layers[self.L-1], 1))
+    
