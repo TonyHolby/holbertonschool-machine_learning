94b9aedfffa15bc5d27f3b3ef518415ad36f51fc
TonyHolby <tonynemoutheholberton@gmail.com>
2025-04-27 11:19:19 +0200

    Fix code in if conditions in 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -25,14 +25,17 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(layers, (list, int)) or len(layers) == 0:
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(map(lambda element: isinstance(
+            element, int) and element > 0, layers)):
             raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for i in range(1, len(layers)):
+        for i in range(1, self.L):
             self.weights['W' + str(i)] = np.random.randn(
                 layers[i], layers[i-1]) * np.sqrt(2 / layers[i-1])
             self.weights['b' + str(i)] = np.zeros((layers[i], 1))
@@ -40,4 +43,3 @@ class DeepNeuralNetwork:
         self.weights['W' + str(self.L)] = np.random.randn(
             layers[self.L-1], layers[self.L-2]) * np.sqrt(2 / layers[self.L-2])
         self.weights['b' + str(self.L)] = np.zeros((layers[self.L-1], 1))
-    
