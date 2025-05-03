38226b1887913d536144d4be014e630443ec7409
TonyHolby <tonynemoutheholberton@gmail.com>
2025-05-03 15:35:32 +0200

    Write code in 5-train.py

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+    A function that trains a model using mini-batch gradient descent.
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+        Trains a model using mini-batch gradient descent
+        and analyze validaiton data.
+
+        Args:
+            network (tensorflow.keras.Model): the model to optimize.
+            data (np.ndarray): a numpy.ndarray of shape (m, nx) containing the
+                input data.
+            labels (np.ndarray): a one-hot numpy.ndarray of shape (m, classes)
+                containing the labels of data.
+            batch_size (int): the size of the batch used for mini-batch
+                gradient descent.
+            epochs (int): the number of passes through data for mini-batch
+                gradient descent.
+            verbose (bool): a boolean that determines if output should be
+                printed during training.
+            shuffle (bool): a boolean that determines whether to shuffle the
+                batches every epoch.
+            validation_data (np.ndarray): the data to validate the model with,
+                if not None.
+
+        Returns:
+            The History object generated after training the model.
+    """
+    train_the_model = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data
+    )
+
+    return train_the_model
