7eb2a4e843d0ef425122e968010931154bb1647d
TonyHolby <tonynemoutheholberton@gmail.com>
2025-05-05 16:47:40 +0200

    Write code in 1-normalize.py

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+    A function that normalizes (standardizes) a matrix.
+"""
+import numpy as np
+
+
+def normalize(X, m, s):
+    """
+        Normalizes (standardizes) a matrix.
+
+        Args:
+            X (np.ndarray): the numpy.ndarray of shape (d, nx) to normalize :
+                - d is the number of data points.
+                - nx is the number of features.
+            m (np.ndarray): a numpy.ndarray of shape (nx,) that contains the
+                mean of all features of X.
+            s (np.ndarray): a numpy.ndarray of shape (nx,) that contains the
+                standard deviation of all features of X.
+
+        Returns:
+            The normalized X matrix.
+    """
+    nx = X.shape[1]
+    X_norm = np.zeros_like(X)
+
+    for feature in range(nx):
+        X_norm[:, feature] = (X[:, feature] - m[feature]
+                              ) / np.sqrt(s[feature] + 1e-8)
+
+    return X_norm
