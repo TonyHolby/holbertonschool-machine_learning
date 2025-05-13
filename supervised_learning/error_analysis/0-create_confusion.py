b91fefdf0f4b96c2fd196d1f21d3982f24fe8754
TonyHolby <tonynemoutheholberton@gmail.com>
2025-05-13 10:53:56 +0200

    Write code in 0-create_confusion.py

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    A function that creates a confusion matrix.
+"""
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+        Creates a confusion matrix.
+
+        Args:
+            labels (np.ndarray): a one-hot numpy.ndarray of shape (m, classes)
+                containing the correct labels for each data point :
+                    m is the number of data points.
+                    classes is the number of classes.
+            logits (np.ndarray): a one-hot numpy.ndarray of shape (m, classes)
+                containing the predicted labels.
+
+        Returns:
+            A confusion numpy.ndarray of shape (classes, classes) with row
+            indices representing the correct labels and column indices
+            representing the predicted labels.
+    """
+    classes = labels.shape[1]
+    confusion_matrix = np.zeros((classes, classes), dtype=int)
+
+    true_labels = np.argmax(labels, axis=1)
+    pred_labels = np.argmax(logits, axis=1)
+
+    for true_index, pred_index in zip(true_labels, pred_labels):
+        confusion_matrix[true_index, pred_index] += 1
+
+    return confusion_matrix
