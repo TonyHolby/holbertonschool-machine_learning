d8a921523c8bb7d5e5816ed49eac9e9c32fc158e
TonyHolby <tonynemoutheholberton@gmail.com>
2025-05-23 15:31:00 +0200

    Fix code in 1-l2_reg_gradient_descent.py

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -48,4 +48,4 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
         weights['W' + str(layer)] -= alpha * dW
-        weights['b' + str(layer)] -= np.roun(alpha * db, 8)
+        weights['b' + str(layer)] -= np.round(alpha * db, 8)
