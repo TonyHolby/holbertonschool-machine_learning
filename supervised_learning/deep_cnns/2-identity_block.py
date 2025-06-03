827e366df054d0777641105e6ad3182f5b2871a3
TonyHolby <tonynemoutheholberton@gmail.com>
2025-06-03 14:37:51 +0200

    Fix code in 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -3,7 +3,7 @@
     A function that builds an identity block as described in Deep Residual
     Learning for Image Recognition (2015).
 """
-from tensorflow import keras
+from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
@@ -12,7 +12,7 @@ def identity_block(A_prev, filters):
         for Image Recognition (2015).
 
         Args:
-            A_prev (np.ndarray): the output from the previous layer.
+            A_prev (tf.Tensor): the output from the previous layer.
             filters (tuple or list): a tuple or list containing F11, F3, F12,
             respectively:
                 F11 is the number of filters in the first 1x1 convolution.
@@ -23,32 +23,32 @@ def identity_block(A_prev, filters):
             The activated output of the identity block.
     """
     F11, F3, F12 = filters
-    he_normal = keras.initializers.HeNormal(seed=0)
+    he_normal = K.initializers.HeNormal(seed=0)
 
-    x = keras.layers.Conv2D(filters=F11,
-                            kernel_size=(1, 1),
-                            padding='same',
-                            kernel_initializer=he_normal)(A_prev)
+    x = K.layers.Conv2D(filters=F11,
+                        kernel_size=(1, 1),
+                        padding='same',
+                        kernel_initializer=he_normal)(A_prev)
 
-    x = keras.layers.BatchNormalization(axis=3)(x)
-    x = keras.layers.Activation('relu')(x)
+    x = K.layers.BatchNormalization(axis=3)(x)
+    x = K.layers.Activation('relu')(x)
 
-    x = keras.layers.Conv2D(filters=F3,
-                            kernel_size=(3, 3),
-                            padding='same',
-                            kernel_initializer=he_normal)(x)
+    x = K.layers.Conv2D(filters=F3,
+                        kernel_size=(3, 3),
+                        padding='same',
+                        kernel_initializer=he_normal)(x)
 
-    x = keras.layers.BatchNormalization(axis=3)(x)
-    x = keras.layers.Activation('relu')(x)
+    x = K.layers.BatchNormalization(axis=3)(x)
+    x = K.layers.Activation('relu')(x)
 
-    x = keras.layers.Conv2D(filters=F12,
-                            kernel_size=(1, 1),
-                            padding='same',
-                            kernel_initializer=he_normal)(x)
+    x = K.layers.Conv2D(filters=F12,
+                        kernel_size=(1, 1),
+                        padding='same',
+                        kernel_initializer=he_normal)(x)
 
-    x = keras.layers.BatchNormalization(axis=3)(x)
+    x = K.layers.BatchNormalization(axis=3)(x)
 
-    x = keras.layers.Add()([x, A_prev])
-    x = keras.layers.Activation('relu')(x)
+    x = K.layers.Add()([x, A_prev])
+    x = K.layers.Activation('relu')(x)
 
     return x
