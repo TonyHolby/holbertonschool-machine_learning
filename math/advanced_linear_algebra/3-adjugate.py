665a070124ddecca62a9589152bc931d47168476
TonyHolby <tonynemoutheholberton@gmail.com>
2025-07-17 10:15:45 +0200

    Write code in 3-adjugate.py

---
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+""" A function that calculates the adjugate matrix of a matrix. """
+
+
+def determinant(matrix):
+    """
+        Calculates the determinant of a matrix.
+
+        Args:
+            matrix (list): a list of lists whose determinant should be
+            calculated.
+
+        Returns:
+            The determinant of matrix.
+    """
+    if matrix == [[]]:
+        det = 1
+
+        return det
+
+    if len(matrix) == 1:
+        det = matrix[0][0]
+
+        return det
+
+    if len(matrix) == 2:
+        det = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+
+        return det
+
+    det = 0
+    for col in range(len(matrix)):
+        sign = (-1) ** col
+        minor = []
+        for row in matrix[1:]:
+            new_row = row[:col] + row[col+1:]
+            minor.append(new_row)
+        det += sign * matrix[0][col] * determinant(minor)
+
+    return det
+
+
+def cofactor(matrix):
+    """
+        Calculates the cofactor matrix of a matrix.
+
+        Args:
+            matrix (list): a list of lists whose cofactor matrix should be
+            calculated.
+
+        Returns:
+            The cofactor matrix of matrix.
+    """
+    if len(matrix) == 1:
+        return [[1]]
+
+    cofactor_matrix = []
+    for index_row in range(len(matrix)):
+        current_cofactor_row = []
+        for index_col in range(len(matrix)):
+            submatrix = []
+            for index, row_list in enumerate(matrix):
+                if index != index_row:
+                    new_row = row_list[:index_col] +\
+                        row_list[index_col + 1:]
+                    submatrix.append(new_row)
+            sign = (-1) ** (index_row + index_col)
+            minor = determinant(submatrix)
+            current_cofactor_row.append(sign * minor)
+        cofactor_matrix.append(current_cofactor_row)
+
+    return cofactor_matrix
+
+
+def adjugate(matrix):
+    """
+        Calculates the adjugate matrix of a matrix.
+
+        Args:
+            matrix (list): a list of lists whose adjugate matrix should be
+            calculated.
+
+        Returns:
+            The adjugate matrix of matrix.
+    """
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a list of lists")
+
+        if len(row) != len(matrix):
+            raise ValueError("matrix must be a non-empty square matrix")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    cofactor_matrix = cofactor(matrix)
+
+    adjugate_matrix = []
+    for row in zip(*cofactor_matrix):
+        row_list = list(row)
+        adjugate_matrix.append(row_list)
+
+    return adjugate_matrix
