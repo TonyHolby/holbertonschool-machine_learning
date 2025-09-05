cfb8c71a062d6ece2bc26502c54dbc7b174d0796
TonyHolby <tonynemoutheholberton@gmail.com>
2025-09-05 09:36:49 +0200

    Write code in 6-bi_backward.py

---
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+    A script that creates a bidirectional cell of an RNN.
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+        A class that represents a bidirectional cell of an RNN.
+    """
+    def __init__(self, i, h, o):
+        """
+            Initializes a bidirectional cell of an RNN.
+
+            Args:
+                i (int): the dimensionality of the data.
+                h (int): the dimensionality of the hidden state.
+                o (int): the dimensionality of the outputs.
+        """
+        self.Whf = np.random.randn(i + h, h)
+        self.bhf = np.zeros((1, h))
+        self.Whb = np.random.randn(i + h, h)
+        self.bhb = np.zeros((1, h))
+        self.Wy = np.random.randn(2 * h, o)
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+            Calculates the hidden state in the forward direction for one time
+            step.
+
+            Args:
+                x_t (np.ndarray): a numpy.ndarray of shape (m, i) that contains
+                    the data input for the cell:
+                        m is the batch size for the data.
+                h_prev (np.ndarray): a numpy.ndarray of shape (m, h) containing
+                    the previous hidden state.
+
+            Returns:
+                h_next, the next hidden state.
+        """
+        forward_concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(forward_concat, self.Whf) + self.bhf)
+
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """
+            Calculates the hidden state in the backward direction for one time
+            step.
+
+            Args:
+                x_t (np.ndarray): a numpy.ndarray of shape (m, i) that contains
+                    the data input for the cell:
+                        m is the batch size for the data.
+                h_next (np.ndarray): a numpy.ndarray of shape (m, h) containing
+                    the next hidden state.
+
+            Returns:
+                h_prev, the previous hidden state.
+        """
+        backward_concat = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.dot(backward_concat, self.Whb) + self.bhb)
+
+        return h_prev
