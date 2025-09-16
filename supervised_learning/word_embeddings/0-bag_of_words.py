ddf07bcc893e9164b929e59390f872a1775b116d
TonyHolby <tonynemoutheholberton@gmail.com>
2025-09-16 18:40:04 +0200

    Fix code in 0-bag_of_word.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -39,7 +39,7 @@ def bag_of_words(sentences, vocab=None):
                                 for word in tokens))
         vocab = np.array(vocab_list)
     else:
-        vocab = list(vocab)
+        vocab = np.array(vocab)
 
     features = vocab
     f = len(features)
