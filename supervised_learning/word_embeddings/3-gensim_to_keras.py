ded25750caffc88c96de24e3e8d255fda8940436
TonyHolby <tonynemoutheholberton@gmail.com>
2025-09-18 11:26:13 +0200

    Fix library import in 3-gensim_to_keras.py

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -2,7 +2,7 @@
 """
     A function that converts a gensim word2vec model to a keras Embedding layer
 """
-import tensorflow
+import tensorflow as tf
 
 
 def gensim_to_keras(model):
@@ -19,7 +19,7 @@ def gensim_to_keras(model):
     vector_size = model.vector_size
     weights = model.wv.vectors
 
-    embedding_layer = tensorflow.keras.layers.Embedding(input_dim=vocab_size,
+    embedding_layer = tf.keras.layers.Embedding(input_dim=vocab_size,
                                                         output_dim=vector_size,
                                                         weights=[weights],
                                                         trainable=True)
