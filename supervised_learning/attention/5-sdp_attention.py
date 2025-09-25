043df0ae67df5acc544044ebe1e62c1f5f35f899
TonyHolby <tonynemoutheholberton@gmail.com>
2025-09-25 11:23:43 +0200

    Write code in 5-sdp_attention.py

---
+++ supervised_learning/attention/5-sdp_attention.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+    A function that calculates the scaled dot product attention.
+"""
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """
+        Calculates the scaled dot product attention.
+
+        Args:
+            Q (tf.Tensor): a tensor with its last two dimensions as
+                (..., seq_len_q, dk) containing the query matrix.
+            K (tf.Tensor): a tensor with its last two dimensions as
+                (..., seq_len_v, dk) containing the key matrix.
+            V (tf.Tensor): a tensor with its last two dimensions as
+                (..., seq_len_v, dv) containing the value matrix.
+            mask (tf.Tensor): a tensor that can be broadcast into
+                (..., seq_len_q, seq_len_v) containing the optional
+                mask, or defaulted to None.
+
+        Returns:
+            output, weights:
+                outputa tensor with its last two dimensions as (...,
+                seq_len_q, dv) containing the scaled dot product attention.
+                weights a tensor with its last two dimensions as (...,
+                seq_len_q, seq_len_v) containing the attention weights.
+    """
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    matmul_scores = tf.matmul(Q, K, transpose_b=True)
+    scaled_scores = matmul_scores / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_scores += (mask * -1e9)
+
+    weights = tf.nn.softmax(scaled_scores, axis=-1)
+    output = tf.matmul(weights, V)
+
+    return output, weights
