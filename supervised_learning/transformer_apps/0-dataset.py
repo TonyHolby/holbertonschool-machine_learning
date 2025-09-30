f680e7031a10137aab91dd23bc23b52e722fb360
TonyHolby <tonynemoutheholberton@gmail.com>
2025-09-30 16:52:40 +0200

    Fix code in 0-dataset.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -19,8 +19,8 @@ class Dataset:
         data = tfds.load("ted_hrlr_translate/pt_to_en", as_supervised=True)
         self.data_train = data['train']
         self.data_valid = data['validation']
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train)
+        self.tokenizer_pt, self.tokenizer_en =\
+            self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
         """
@@ -38,20 +38,9 @@ class Dataset:
                     tokenizer_pt is the Portuguese tokenizer.
                     tokenizer_en is the English tokenizer.
         """
-        pt_sentences, en_sentences = [], []
-
-        for pt, en in tfds.as_numpy(data):
-            pt_sentences.append(pt.decode('utf-8'))
-            en_sentences.append(en.decode('utf-8'))
-
-        vocab_size = 2 ** 13
-
-        tokenizer_pt = \
-            tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-                pt_sentences, target_vocab_size=vocab_size)
-
-        tokenizer_en = \
-            tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-                en_sentences, target_vocab_size=vocab_size)
+        tokenizer_pt = transformers.BertTokenizer.from_pretrained(
+            "neuralmind/bert-base-portuguese-cased")
+        tokenizer_en = transformers.BertTokenizer.from_pretrained(
+            "bert-base-uncased")
 
         return tokenizer_pt, tokenizer_en
