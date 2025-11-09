5fb3585c7cb5c78b7b556734800c8ab91e0cfdf1
TonyHolby <tonynemoutheholberton@gmail.com>
2025-11-09 16:32:33 +0100

    Fix code in 2-sarsa_lambtha.py

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -54,9 +54,9 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
             td_target = reward +\
                 gamma * Q[next_state, next_action] * (not done)
             delta = td_target - Q[state, action]
+            E *= gamma * lambtha
             E[state, action] += 1
             Q += alpha * delta * E
-            E *= gamma * lambtha
             state, action = next_state, next_action
 
             if done:
